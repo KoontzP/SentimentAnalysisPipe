6aef1bf59d71cd1f31b4157ca822d66ebf900447
Pontus Koontz <pontus.koontz@gmail.com>
2024-04-12 12:38:21 +0200

    Start of main, added files for webbsite

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from transformers import pipeline
 import torch
 import torch.nn.functional as F
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-classifier = pipeline("sentiment-analysis")
+model_name = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+x_train = ["Hello", "Great news, you are cancer free"]
+
+batch = tokenizer(x_train, padding=True, truncation=True, max_length=512, return_tensors="pt")
+
+with torch.no_grad():
+    outputs = model(**batch)
+    label_ids = torch.argmax(outputs.logits, dim=1)
+    print(label_ids)
+    labels = [model.config.id2label[label_ids] for label_ids in label_ids.tolist()]
+    print(labels)
+
